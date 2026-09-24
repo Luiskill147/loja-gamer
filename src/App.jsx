da17f891dae5832895918087da2ac4670710f438
Laboratório FIAP <labsfiap@fiap.com.br>
2026-09-24 11:48:58 -0300

    feat:Codificando e Estilizando o Header.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,24 @@
-import { browserRouter as Router,Router,Router } from "react-Router-dom";
-import Heart from './componets/Heart'
+import { browserRouter as Router, Routes, Route } from "react-Router-dom";
+import  Header  from './componets/Header'
 import Footer from './componets/Footer'
 import Home from './pages/Contato'
 import Jogos from './pages/Jogos'
 import Login from './pages/Login'
 import Error from './pages/Error'
 
-const app =() => {
-  return(
+const app = () => {
+  return (
     <Router>
       <div className="min-h-screen flex flex-col justify-between bg-[#141414] pt-4">
         <Routes>
-          <Route path="/"element={<Home/>}/>
-          <Route path="/jogos"element={<Jogos/>}/>
-          <Route path="/contato"element={<Contato/>}/>
-          <Route path="/login"element={<Login/>}/>
-          <Route path="*"element={<Error/>}/>
-        </Routes> 
+          <Route path="/" element={<Home />} />
+          <Route path="/jogos" element={<Jogos />} />
+          <Route path="/contato" element={<Contato />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="*" element={<Error />} />
+        </Routes>
       </div>
-      <Footer/>
+      <Footer />
     </Router>
   )
 }
